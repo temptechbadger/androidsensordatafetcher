8719d140bfba096fd4eb7d802762f968021ec608
Patrick Schiller <patrick.schiller@dacs-bund.de>
2025-05-22 14:54:10 +0200

    use event timestamp for increased accuracy

--- app/src/main/java/com/example/androidsensordatafetcher/SensorService.kt
+++ app/src/main/java/com/example/androidsensordatafetcher/SensorService.kt
@@ -76,7 +76,8 @@ class SensorService : Service() {
                     writer.write(event.sensor.name)
                     writer.write(",")
 //                    event.timestamp returns uptime, not world time
-                    writer.write(System.currentTimeMillis().toString())
+//                    use event.timestamp for increased accuracy
+                    writer.write(event.timestamp.toString())
                     writer.write(",")
                     writer.write(event.values.joinToString())
                     writer.newLine()
